eb71ee2e32e32d10c9f184810d040297c435d12e
adam <adam@gmail.com>
2024-05-10 15:33:31 +0100

    page simple

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+    {
+      path : "/",
+      element : <Root/>
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
